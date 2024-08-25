4a67bb202783d05cdefafcb1f102d67798eefec6
PanjiSri <neiabaraja04@gmail.com>
2024-08-25 14:40:56 +0700

    feat : gamenya sudah, tinggal implementasi Q Learning dan SARSA

---
+++ game.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+class PermainanSederhana:
+    def __init__(self):
+        self.panjang_papan = 10
+        self.posisi_lubang = 0
+        self.posisi_apel = 9
+        self.posisi_awal = 2
+        self.posisi_pemain = self.posisi_awal
+        
+        self.poin = 0
+        self.poin_menang = 500
+        self.poin_kalah = -200
+        
+        self.hadiah = {'lubang': -100, 'apel': 100, 'langkah': -1}
+        
+        self.permainan_selesai = False
+        
+    def reset(self):
+        self.posisi_pemain = self.posisi_awal
+        self.poin = 0
+        self.permainan_selesai = False
+        return self.posisi_pemain, self.poin
+    
+    def langkah(self, aksi):
+        if aksi == 'kiri':
+            self.posisi_pemain -= 1
+        elif aksi == 'kanan':
+            self.posisi_pemain += 1
+        else:
+            print("Aksi tidak valid! Aksi harus 'kiri' atau 'kanan'.")
+            return self.posisi_pemain, self.poin, self.permainan_selesai
+        
+        self.posisi_pemain = max(0, min(self.posisi_pemain, self.panjang_papan - 1))
+        
+        if self.posisi_pemain == self.posisi_lubang:
+            self.poin += self.hadiah['lubang']
+            self.posisi_pemain = self.posisi_awal
+        elif self.posisi_pemain == self.posisi_apel:
+            self.poin += self.hadiah['apel']
+            self.posisi_pemain = self.posisi_awal
+        else:
+            self.poin += self.hadiah['langkah']
+        
+        if self.poin >= self.poin_menang:
+            self.permainan_selesai = True
+            print("Pemain menang!")
+        elif self.poin <= self.poin_kalah:
+            self.permainan_selesai = True
+            print("Pemain kalah!")
+        
+        return self.posisi_pemain, self.poin, self.permainan_selesai
+    
+    def tampilkan(self):
+        papan = [' '] * self.panjang_papan
+        papan[self.posisi_lubang] = 'L'
+        papan[self.posisi_apel] = 'A'
+        papan[self.posisi_pemain] = 'P'
+        
+        print("_" * 41)
+        print("|", end="")
+        for i in papan:
+            print(f" {i} |", end="")
+        print("\n" + "_" * 41)
+        print(f"Poin: {self.poin}\n")
+
+permainan = PermainanSederhana()
+permainan.tampilkan()
+
+while not permainan.permainan_selesai:
+    aksi = input("Masukkan aksi ('kiri' atau 'kanan'): ").strip().lower()
+    _, _, permainan_selesai = permainan.langkah(aksi)
+    permainan.tampilkan()
